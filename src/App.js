e284dc161bde458dcdfd235db00d7029147cb83a
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-03-09 17:53:07 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import data from './data.js';
 
 export default function App() {
-  const [count,setCount]=useState([0]);
-  const submitHandler = (e)=>{e.preventDefault()}
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
-    <section>
-      <h2>lorem ipsum project setup</h2>
-      <form>
-        <input type="number" onClick={}/>
-        <button class={} onClick={submitHandler}></button>
+    <section className="section-center">
+      <h2>Tried of boring lorem ipsum</h2>
+      <form className="lorem-form" onSubmit={submitHandler}>
+        <label htmlFor="amount">Paragraph:</label>
+        <input
+          type="number"
+          id="amount"
+          value={count}
+          name="amount"
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn" onClick={submitHandler}>
+          Generate
+        </button>
       </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
     </section>
   );
 }
